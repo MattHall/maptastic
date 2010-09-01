79036630afd99534c00aa7bdb118caf5bd3dffa9
lostboy <paul.crabtree@gmail.com>
2010-09-01 18:25:05 +0800

    refactored javascript logic into external js file and included rake task for install (rake maptastic:install). corrected map id generation

---
+++ assets/javascripts/maptastic.js
@@ -0,0 +1,45 @@
+MaptasticMap = {
+	marker: null,
+	options: { mapId: null, latitudeInput: null, longitudeInput: null }, // default options
+	setMarker: function(map, location) {
+		if (!this.marker) this.createMarker(map, location);
+		this.marker.setPosition(location);
+	},
+	createMarker: function(map, location) {
+		this.marker = new google.maps.Marker({
+			map: map,
+			title: 'Drag to reposition',
+			draggable: true
+		});
+		var clazz = this;
+		google.maps.event.addListener(this.marker, 'dragend', function(event) {
+			clazz.updateInputs(event);
+		});
+	},
+	updateInputs: function(event) {
+		document.getElementById(this.options.latInput).value = event.latLng.lat();
+		document.getElementById(this.options.lngInput).value = event.latLng.lng();
+	},
+	init: function(options) {
+		this.options = options;
+		var map = new google.maps.Map(document.getElementById(this.options.mapId), {
+			zoom: 6,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		});
+		if (document.getElementById(this.options.latInput).value && document.getElementById(this.options.lngInput).value) {
+			var location = new google.maps.LatLng(document.getElementById(this.options.latInput).value, document.getElementById(this.options.lngInput).value);
+			map.setCenter(location);
+			this.setMarker(map, location);
+		} else if (navigator.geolocation) {
+  			navigator.geolocation.getCurrentPosition(function(position) {
+    			initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+    			map.setCenter(initialLocation);
+  			});
+		}
+		var clazz = this;
+		google.maps.event.addListener(map, 'click', function(event){
+			clazz.setMarker(map, event.latLng);
+			clazz.updateInputs(event);
+		});
+	}
+}
--- tasks/maptastic_tasks.rake
+++ tasks/maptastic_tasks.rake
@@ -1,4 +1,8 @@
-# desc "Explaining what the task does"
-# task :maptastic do
-#   # Task goes here
-# end
+include FileUtils
+
+namespace :maptastic do
+  desc 'Install maptastic javascript'
+  task :install => :environment do
+    cp_r "#{File.join(File.dirname(__FILE__), '../assets')}/javascripts/maptastic.js", "#{Rails.root}/public/javascripts"
+  end
+end
